a5925cb26ebba7837b526dababfcf50dfa291a9b
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-04-07 16:12:37 -0300

    Config next auth

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,28 +1,36 @@
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { SessionProvider } from 'next-auth/react'
 import { Nunito } from 'next/font/google'
 
 import { globalStyles } from '@/styles/global'
-import { useRouter } from 'next/router'
 import DefaultLayout from '@/layouts/DefaultLayout'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const router = useRouter()
 
   if (router.pathname === '/login') {
     return (
       <div className={nunito.className}>
-        <Component {...pageProps} />
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </div>
     )
   }
 
   return (
     <DefaultLayout className={nunito.className}>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </DefaultLayout>
   )
 }
