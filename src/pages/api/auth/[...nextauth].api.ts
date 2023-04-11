a09ae44cd684a79794ef57b49a51c240726e5e2b
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-04-11 18:49:14 -0300

    Add prisma adapter config

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,16 +1,37 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import GitHubProvider, { GithubProfile } from 'next-auth/providers/github'
+import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: '',
+          avatar_url: profile.picture,
+        }
+      },
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+
+      profile(profile: GithubProfile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || '',
+          email: '',
+          avatar_url: profile.avatar_url,
+        }
+      },
     }),
   ],
 }
