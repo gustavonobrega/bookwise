b3915c1bab0bb5a287050acf68585f00af68e91b
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-04-18 18:35:49 -0300

    Update Ratings component to get dynamic rate

--- src/components/Ratings/index.tsx
+++ src/components/Ratings/index.tsx
@@ -3,13 +3,18 @@ import { RatingsContainer } from './styles'
 
 interface RatingsProps {
   size: 16 | 20 | 28
+  rate: number
 }
 
-export function Ratings({ size }: RatingsProps) {
+export function Ratings({ size, rate }: RatingsProps) {
+  const stars = Array.from({ length: 5 }, (_, i) =>
+    i + 1 <= rate ? 'fill' : 'regular',
+  )
+
   return (
     <RatingsContainer>
-      {Array.from({ length: 5 }, (_, i) => (
-        <Star key={i} size={size} />
+      {stars.map((starWeight, i) => (
+        <Star key={i} size={size} weight={starWeight} />
       ))}
     </RatingsContainer>
   )
