4c1f6338839465761440f0f97d984addac62fac6
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-04-20 16:56:46 -0300

    Update Login page to redirect logged users

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { signIn, useSession } from 'next-auth/react'
 
 import hero from '../../assets/hero.png'
 import google from '../../assets/google-icon.svg'
@@ -11,8 +12,18 @@ import {
   ButtonsWrapper,
   SignInButton,
 } from './styles'
+import { useRouter } from 'next/router'
 
 export default function Login() {
+  const session = useSession()
+  const router = useRouter()
+
+  const isSignedIn = session.status === 'authenticated'
+
+  if (isSignedIn) {
+    router.push('/')
+  }
+
   return (
     <LoginContainer>
       <Image src={hero} alt="Book Wise logo" quality={100} priority />
@@ -22,15 +33,15 @@ export default function Login() {
           <h2>Faça seu login ou acesse como visitante.</h2>
 
           <ButtonsWrapper>
-            <SignInButton>
+            <SignInButton onClick={() => signIn('google')}>
               <Image src={google} alt="Google icon" />
               Entrar com Google
             </SignInButton>
-            <SignInButton>
+            <SignInButton onClick={() => signIn('github')}>
               <Image src={github} alt="Github icon" />
               Entrar com GitHub
             </SignInButton>
-            <SignInButton>
+            <SignInButton onClick={() => router.push('/')}>
               <Image src={guest} alt="Guest icon" />
               Acessar como visitante
             </SignInButton>
