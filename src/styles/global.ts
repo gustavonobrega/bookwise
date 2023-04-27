032544dfc20b4c00844aec4640d39ccea1388948
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-04-27 19:31:32 -0300

    Add global scrollbar style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,11 +5,25 @@ export const globalStyles = globalCss({
     boxSizing: 'border-box',
     padding: 0,
     margin: 0,
+
+    '&::-webkit-scrollbar': {
+      width: 6,
+    },
+
+    '&::-webkit-scrollbar-thumb': {
+      background: '$gray600',
+      borderRadius: 999,
+    },
+
+    '&::-webkit-scrollbar-track': {
+      background: '$gray700',
+    },
   },
 
   body: {
     backgroundColor: '$gray800',
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
+    fontFamily: '$default',
   },
 })
