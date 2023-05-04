af91a07476a2c60178555e43e259d4f06484ebdd
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2023-05-04 16:38:13 -0300

    Add ratings api route post method

--- src/pages/api/ratings/index.api.ts
+++ src/pages/api/ratings/index.api.ts
@@ -1,23 +1,73 @@
+/* eslint-disable camelcase */
 import { prisma } from '@/lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+const createRatingBodySchema = z.object({
+  rate: z.number().min(1).max(5),
+  description: z.string().min(1).max(490),
+  book_id: z.string(),
+  user_id: z.string().uuid(),
+})
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== 'GET') {
+  const allowedMethods = ['GET', 'POST']
+
+  if (!allowedMethods.includes(req.method!)) {
     return res.status(405).end()
   }
 
-  const recentRatings = await prisma.rating.findMany({
-    include: {
-      book: true,
-      user: true,
-    },
-    orderBy: {
-      created_at: 'desc',
-    },
-  })
+  if (req.method === 'GET') {
+    const recentRatings = await prisma.rating.findMany({
+      include: {
+        book: true,
+        user: true,
+      },
+      orderBy: {
+        created_at: 'desc',
+      },
+    })
 
-  return res.json(recentRatings)
+    return res.json(recentRatings)
+  }
+
+  if (req.method === 'POST') {
+    const { rate, description, book_id, user_id } =
+      createRatingBodySchema.parse(req.body)
+
+    const ratingAlreadyExists = await prisma.rating.findFirst({
+      where: {
+        book_id,
+        user_id,
+      },
+    })
+
+    if (ratingAlreadyExists) {
+      return res.status(400).json({
+        message: 'You already have rated this book',
+      })
+    }
+
+    const rating = await prisma.rating.create({
+      data: {
+        rate,
+        description,
+        book: {
+          connect: {
+            id: book_id,
+          },
+        },
+        user: {
+          connect: {
+            id: user_id,
+          },
+        },
+      },
+    })
+
+    return res.status(201).json(rating)
+  }
 }
